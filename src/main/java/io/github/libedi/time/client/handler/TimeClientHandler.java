d43588ae0162e0a4e257c17e6c6753346ab5c769
Sangjun, Park <libedi@gmail.com>
2024-04-27 05:33:02 +0900

    수신 데이터 조각화에 따른 첫번째 솔루션

--- src/main/java/io/github/libedi/time/client/handler/TimeClientHandler.java
+++ src/main/java/io/github/libedi/time/client/handler/TimeClientHandler.java
@@ -16,15 +16,52 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
 
+	private ByteBuf buf; // 2.
+
+	/*
+	 * 2. ChannelHandler의 수명주기 메소드, handlerAdded()와 handlerRemoved()를 통해 초기화 작업 수행
+	 * 가능
+	 */
+	@Override
+	public void handlerAdded(final ChannelHandlerContext ctx) throws Exception {
+		buf = ctx.alloc().buffer(4);
+	}
+
+	@Override
+	public void handlerRemoved(final ChannelHandlerContext ctx) throws Exception {
+		buf.release();
+		buf = null;
+	}
+
 	@Override
 	public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
+		/*
+		 * 1. TCP/IP와 같은 스트림 기반 전송에서 수신된 데이터는 패킷 단위가 아닌 바이트 단위로 수신된다.
+		 * 따라서 원하는 패킷 길이로 오지 않아 조각화 될 가능성이 있어,
+		 * 아래 코드는 IndexOutOfBoundsException가 발생할 수 있다.
+		 */
 		final ByteBuf m = (ByteBuf) msg;
-		try {
+		/*
+		 * 3. 조각화를 방지하기 이해 원하는 크기의 데이터로 수신 데이터 누적
+		 */
+		buf.writeBytes(m);
+		// 4.
+//		try {
+//			final long currentTimeMills = (m.readUnsignedInt() - 2208988800L) * 1000L;
+//			log.debug("{}", new Date(currentTimeMills));
+//			ctx.close();
+//		} finally {
+//			m.release();
+//		}
+		m.release();
+
+		/*
+		 * 4. 충분한 데이터가 있는지 확인하고 비즈니스 로직 수행
+		 */
+		if (buf.readableBytes() >= 4) {
 			final long currentTimeMills = (m.readUnsignedInt() - 2208988800L) * 1000L;
 			log.debug("{}", new Date(currentTimeMills));
 			ctx.close();
-		} finally {
-			m.release();
 		}
 	}
 
